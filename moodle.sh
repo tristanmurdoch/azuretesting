c24bff908d6710cbae6e3ad6089acd3312ab5386
tristanmurdoch <t.mackay@murdoch.edu.au>
2020-04-14 18:51:52 +0800

    Update moodle.sh

--- moodle.sh
+++ moodle.sh
@@ -7,7 +7,7 @@ set -ex
 sudo mkdir -p /moodle
 
 # Moodle requirements
-    sudo apt-get -y update > /dev/null
+    sudo apt-get update > /dev/null
     sudo apt-get install -y  graphviz aspell php-common php-soap php-json php-redis > /tmp/apt2.log
     sudo apt-get install -y  php-bcmath php-gd php-xmlrpc php-intl php-xml php-bz2 php-pear php-mbstring php-dev mcrypt > /tmp/apt3.log
 
